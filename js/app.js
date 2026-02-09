7b086eefb17968217f21ba8f63d5a9f4aebe9124
Sadeep Khanal <sadeepkhanal343@gmail.com>
2026-02-09 19:39:25 +0545

    updating app.js
    includes edit completed function to toggle an item's completed state and re render UI

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var items = groceryItems;
+
+// Render App
+function render() {
+  var $app = $("#app");
+  $app.empty();
+
+  var $itemsElement = createItems(items);
+  $app.append($itemsElement);
+}
+
+// Initialize App
+$(document).ready(function () {
+  render();
+});
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  render();
+}
